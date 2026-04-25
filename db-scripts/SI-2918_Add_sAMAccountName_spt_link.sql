989b4c05136f64728f7bcae23fdcf8e37b25215b
rachidbourass <rachid.bourass.resume@gmail.com>
2026-04-25 14:28:09 -0400

    sss
    ss

--- db-scripts/SI-2918_Add_sAMAccountName_spt_link.sql
+++
@@ -1,7 +0,0 @@
--- 1. Add the new custom column
-ALTER TABLE [identityiq].[identityiq].[spt_link] ADD sam_account_name NVARCHAR(128);
-GO
-
--- 2. Create the index using your environment's naming convention
-CREATE NONCLUSTERED INDEX spt_link_samAccountName_ci ON [identityiq].[identityiq].[spt_link](sam_account_name);
-GO
