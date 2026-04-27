d92d7bea0205b04517635ccbbf19a6773fd38e78
rachidbourass <rachid.bourass.resume@gmail.com>
2026-04-26 21:55:12 -0400

    Add files via upload

---
+++ buildDEV.bat
@@ -0,0 +1,8 @@
+SET SPTARGET=dev
+### The below executable jar is used to generate env.include.properties files used by the ant script to build subset
+### of deployment artifacts
+##java -jar admintools.jar org.ascn.admintools.BuildIncludeFile  "target" "todaydate" "workkingDir"
+##java -jar admintools.jar org.ascn.admintools.BuildIncludeFile  "dev" "MMddyyyy" "<Root Path of the SSB Folder>"
+## todayDate = Get-Date -UFormat %m%d%Y;
+##Example: java -jar admintools.jar org.ascn.admintools.BuildIncludeFile  "%SPTARGET%" "todayDate" "D:\\IdentityIIQ-Projects\\"
+ant clean main import-custom-subset
